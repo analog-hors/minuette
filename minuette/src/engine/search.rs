ab4a893a28f68ff80f4d1a1640b3f48bc83773d3
analog-hors <44050761+analog-hors@users.noreply.github.com>
2024-07-30 19:25:32 +1000

    Implement alpha-beta pruning
    bench: 2365765
    Score of minuette-alpha-beta vs minuette-main: 76 - 2 - 4  [0.951] 82
    ...      minuette-alpha-beta playing White: 36 - 2 - 3  [0.915] 41
    ...      minuette-alpha-beta playing Black: 40 - 0 - 1  [0.988] 41
    ...      White vs Black: 36 - 42 - 4  [0.463] 82
    Elo difference: 516.0 +/- 208.0, LOS: 100.0 %, DrawRatio: 4.9 %
    SPRT: llr 2.92 (101.0%), lbound -2.25, ubound 2.89 - H1 was accepted

--- minuette/src/engine/search.rs
+++ minuette/src/engine/search.rs
@@ -67,7 +67,7 @@ impl Search {
     pub fn start(mut self, init_pos: &Board, moves_played: &[Move], on_iter: &mut dyn FnMut(SearchInfo)) {
         let mut board = BoardStack::new(init_pos, moves_played);
         for target_depth in 1..=self.max_depth {
-            let Some(eval) = self.negamax(&mut board, target_depth, 0) else {
+            let Some(eval) = self.negamax(&mut board, -INFINITY, INFINITY, target_depth, 0) else {
                 break;
             };
 
@@ -85,7 +85,9 @@ impl Search {
         }
     }
 
-    fn negamax(&mut self, board: &mut BoardStack, depth: u8, ply: u8) -> Option<i16> {
+    fn negamax(&mut self, board: &mut BoardStack, mut alpha: i16, beta: i16, depth: u8, ply: u8) -> Option<i16> {
+        assert!(alpha < beta);
+
         self.nodes += 1;
 
         if self.nodes % 1024 == 0 && self.best_move.is_some() && self.search_start.elapsed() >= self.hard_limit {
@@ -116,14 +118,20 @@ impl Search {
         let mut best_score = -INFINITY;
         for mv in movelist {
             board.play_unchecked(mv);
-            let child_score = -self.negamax(board, depth - 1, ply + 1)?;
+            let child_score = -self.negamax(board, -beta, -alpha, depth - 1, ply + 1)?;
             board.undo();
 
             if child_score > best_score {
                 best_move = Some(mv);
                 best_score = child_score;
+                alpha = alpha.max(child_score);
+            }
+
+            if alpha >= beta {
+                break;
             }
         }
+        assert!(best_move.is_some());
 
         if ply == 0 {
             self.best_move = best_move;
